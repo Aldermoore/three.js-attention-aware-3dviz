810b9078c099995c73ac6cd35b33e01801669a74
Johannes Ellemose <au616626@uni.au.dk>
2024-03-08 10:48:16 +0100

    trying some shit

--- src/Visualization/Visualization.js
+++ src/Visualization/Visualization.js
@@ -294,9 +294,9 @@ class Visualization {
     scene.add( group );
 
     const mesh = new HTMLMesh( gui.domElement );
-    mesh.position.x = - 1;
+    mesh.position.x = 0;
     mesh.position.y = 0;
-    mesh.position.z = - 1;
+    mesh.position.z = 0;
     // mesh.rotation.y = Math.PI / 4;
     mesh.scale.setScalar( 2 );
     group.add( mesh );

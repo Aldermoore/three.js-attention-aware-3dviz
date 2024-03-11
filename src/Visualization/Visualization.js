e31726ff7420874257a9858a6d09f63db28abe32
Johannes Ellemose <au616626@uni.au.dk>
2024-03-11 11:57:55 +0100

    trying something completely different

--- src/Visualization/Visualization.js
+++ src/Visualization/Visualization.js
@@ -569,7 +569,7 @@ class Visualization {
 
 
   isHoveringAreaBuffer(buffer) {
-    let subBuffer = this.findAreaFromArray(buffer, params.areaPickSize, 1000, 850 ); // mousePick.x, mousePick.y); quest 3 res: 1680x1760
+    let subBuffer = this.findAreaFromArray(buffer, params.areaPickSize, 1000, 450 ); // mousePick.x, mousePick.y); quest 3 res: 1680x1760
     return subBuffer;
   }
 

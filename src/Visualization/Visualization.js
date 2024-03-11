3382eaeb2ae0a3197765f54023da0871b0463ebd
Johannes Ellemose <au616626@uni.au.dk>
2024-03-11 11:24:12 +0100

    fine tuning hovering buffer location - getting close

--- src/Visualization/Visualization.js
+++ src/Visualization/Visualization.js
@@ -569,7 +569,7 @@ class Visualization {
 
 
   isHoveringAreaBuffer(buffer) {
-    let subBuffer = this.findAreaFromArray(buffer, params.areaPickSize, 1000, 550 ); // mousePick.x, mousePick.y); quest 3 res: 1680x1760
+    let subBuffer = this.findAreaFromArray(buffer, params.areaPickSize, 1000, 50 ); // mousePick.x, mousePick.y); quest 3 res: 1680x1760
     return subBuffer;
   }
 

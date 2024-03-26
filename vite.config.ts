7c664b1ee5b634e93111eb2f34f2f06cd82467ff
Johannes Ellemose <ellemose@cs.au.dk>
2024-03-26 10:15:14 +0100

    rounting should be fixed now with a vite config update

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,34 @@
 import basicSsl from '@vitejs/plugin-basic-ssl'
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
 
-export default {
+// export default {
 
-  base: './',
+//   plugins: [
+//     basicSsl()
+//   ]
+// }
+
+
+
+
+export default defineConfig({
+  build: { rollupOptions: {
+      input: { 
+        main: resolve(__dirname, 'index.html'),
+        conditionA: resolve(__dirname, 'conditionA.html'),
+        conditionB: resolve(__dirname, 'conditionB.html'),
+        conditionC: resolve(__dirname, 'conditionC.html'),
+        conditionD: resolve(__dirname, 'conditionD.html'),
+        conditionE: resolve(__dirname, 'conditionE.html'),
+        conditionF: resolve(__dirname, 'conditionF.html'),
+        conditionG: resolve(__dirname, 'conditionH.html'),
+        conditionH: resolve(__dirname, 'conditionH.html'),
+      },
+    },
+  },
 
   plugins: [
     basicSsl()
   ]
-}
+})
